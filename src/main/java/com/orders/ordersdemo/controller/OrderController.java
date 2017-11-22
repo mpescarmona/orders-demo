17f0ac8af60ac74fa1df66cd8ff8c4daa68af424
Mario Pescarmona <mpescarmona@nisum.com>
2017-11-22 18:04:08 -0300

    Update Rest controller to retrieve the required status codes

--- src/main/java/com/orders/ordersdemo/controller/OrderController.java
+++ src/main/java/com/orders/ordersdemo/controller/OrderController.java
@@ -1,12 +1,15 @@
 package com.orders.ordersdemo.controller;
 
 import com.orders.ordersdemo.domain.Order;
+import com.orders.ordersdemo.domain.Vendor;
+import com.orders.ordersdemo.exception.OrderNotFoundException;
 import com.orders.ordersdemo.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController()
 @RequestMapping(value="/orders")
@@ -16,34 +19,78 @@ public class OrderController {
     OrderRepository orderRepository;
 
     @RequestMapping(method = RequestMethod.POST)
-    public Order saveOrder(@RequestBody Order order) {
+    public ResponseEntity < String > saveOrder(@RequestBody Order order) {
 
-        Order existingOrder = orderRepository.findByOrderNumber(order.getOrderNumber());
-        if (existingOrder != null) {
-            if (order.getStatus() != null) {
-                existingOrder.setStatus(order.getStatus());
-            }
-            if (order.getSourceSystem() != null) {
-                existingOrder.setSourceSystem(order.getSourceSystem());
-            }
-            if (order.getVendor() != null) {
-                existingOrder.setVendor(order.getVendor());
-            }
-            existingOrder.setUpdatedTimestamp(new Date());
-            orderRepository.save(existingOrder);
+        if (isValid(order)) {
+            Order existingOrder = orderRepository.findByOrderNumber(order.getOrderNumber());
+            if (existingOrder != null) {
+                if (order.getStatus() != null) {
+                    existingOrder.setStatus(order.getStatus());
+                }
+                if (order.getSourceSystem() != null) {
+                    existingOrder.setSourceSystem(order.getSourceSystem());
+                }
+                if (order.getVendor() != null) {
+                    existingOrder.setVendor(order.getVendor());
+                }
+                existingOrder.setUpdatedTimestamp(new Date());
 
-            return existingOrder;
+                orderRepository.save(existingOrder);
+            } else {
+                order.setCreatedTimestamp(new Date());
 
+                orderRepository.save(order);
+            }
+
+            return ResponseEntity.status(HttpStatus.CREATED).build();
         } else {
-            order.setCreatedTimestamp(new Date());
-            orderRepository.save(order);
-
-            return order;
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
         }
     }
 
     @RequestMapping(method=RequestMethod.GET)
     public Order show(@RequestParam(name = "orderNumber") String orderNumber) {
-        return orderRepository.findByOrderNumber(orderNumber);
+
+        Order foundOrder = orderRepository.findByOrderNumber(orderNumber);
+        if (foundOrder != null) {
+            return foundOrder;
+        } else {
+            throw new OrderNotFoundException(orderNumber);
+        }
+    }
+
+    private boolean isValid(Order order) {
+        boolean result = true;
+        if (order == null) {
+            result = false;
+        }
+        if (result && order.getOrderNumber() == null) {
+            result = false;
+        }
+        if (result && order.getStatus() == null) {
+            result = false;
+        }
+        if (result && order.getSourceSystem() == null) {
+            result = false;
+        }
+        if (result && !isValid(order.getVendor())) {
+            result = false;
+        }
+
+        return result;
+    }
+
+    private boolean isValid(Vendor vendor) {
+        boolean result = true;
+        if (vendor == null) {
+            result = false;
+        }
+        if (result && vendor.getName() == null) {
+            result = false;
+        }
+        if (result && vendor.getNumber() == null) {
+            result = false;
+        }
+        return result;
     }
 }
