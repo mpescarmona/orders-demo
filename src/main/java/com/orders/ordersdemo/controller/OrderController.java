d7a4e862b525b5b577f3d46edc73abaf62f14ce2
Mario Pescarmona <mpescarmona@nisum.com>
2017-11-22 12:29:47 -0300

    Implementing POST method

--- src/main/java/com/orders/ordersdemo/controller/OrderController.java
+++ src/main/java/com/orders/ordersdemo/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.orders.ordersdemo.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController()
@@ -13,6 +14,14 @@ public class OrderController {
 
     @Autowired
     OrderRepository orderRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Order saveOrder(@RequestBody Order order) {
+        order.setCreatedTimestamp(new Date());
+        orderRepository.save(order);
+
+        return order;
+    }
 
     @RequestMapping(method=RequestMethod.GET)
     public Order show(@RequestParam(name = "orderNumber") String orderNumber) {
