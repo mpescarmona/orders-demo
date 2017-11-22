e062f23db49bf3b912547625164bf4a6ba0a688a
Mario Pescarmona <mpescarmona@nisum.com>
2017-11-22 10:58:02 -0300

    Initial commit

---
+++ src/main/java/com/orders/ordersdemo/OrdersDemoApplication.java
@@ -0,0 +1,12 @@
+package com.orders.ordersdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersDemoApplication.class, args);
+	}
+}
